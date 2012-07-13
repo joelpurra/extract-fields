cc55cea00f29c836cdc9fc80010dc405b423835a
Joel Purra <code@joelpurra.com>
2012-07-13 17:12:06 -0400

    Unused code for prepending text to files

--- extract-field-names.sh
+++ extract-field-names.sh
@@ -12,4 +12,18 @@ done
 
 cat `find . -name '*.field-names.txt' -print` | perl -ne '0==$H{$_}-- or print' > `dirname ${forms[0]}`/shared-field-names.txt
 
+# Optional
+# Prepend the file with YAML front matter, to have
+# Jekyll regenerate the _site/ output for us.
+# From
+# 	http://stackoverflow.com/questions/54365/prepend-to-a-file-one-liner-shell
+# 	http://stackoverflow.com/a/3272296/
+# 	http://stackoverflow.com/a/5297363/
+# TODO: build new array
+#for form in "field-names-${forms[0]}" "field-names-${forms[1]}" "shared-field-names"
+#do
+#	:
+#	printf "%s\n" '0a' '---' '---' '.' 'wq' | ed -s $form.txt
+#done
+
 cd - > /dev/null
